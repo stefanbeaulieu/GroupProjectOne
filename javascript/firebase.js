ed2614b7cc33afc284f725493b62ee089cc5a0f8
kfpedraza <kfpedraza@gmail.com>
2016-11-08 21:11:15 -0500

    #1 initialize Firebase/Config

--- javascript/firebase.js
+++ javascript/firebase.js
@@ -1,14 +1,15 @@
 // HTML script source
-// <script src="https://www.gstatic.com/firebasejs/3.6.0/firebase.js"></script>
+//<script src="https://www.gstatic.com/firebasejs/3.6.0/firebase.js"></script>
 
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyCW5J7X-v5m9yX2_F9eAHzyGIHl6OBfgQw",
     authDomain: "giflygroupproject.firebaseapp.com",
     databaseURL: "https://giflygroupproject.firebaseio.com",
     storageBucket: "giflygroupproject.appspot.com",
     messagingSenderId: "1047383400502"
   };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+
+var database = firebase.database();
