982b3173603b78f3b00e525d7ea9921acc94a0ca
kfpedraza <kfpedraza@gmail.com>
2016-11-10 20:11:46 -0500

    #5 Firebase Create User

--- javascript/firebase.js
+++ javascript/firebase.js
@@ -13,3 +13,34 @@ firebase.initializeApp(config);
 
 
 var database = firebase.database();
+var auth = firebase.auth();
+
+//Create new user
+$("#submitButton").on('click', function() {
+
+	var email = $("#InputEmail").val().trim();
+	var password = $("#InputPassword").val().trim();
+
+	//Create new user
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+
+ 	// Handle Errors here.
+ 	var errorCode = error.code;
+ 	var errorMessage = error.message;
+
+ 	//Error message
+ 	if (error) {
+ 		alert(errorMessage);
+ 	}
+
+	});//end firebase function
+
+	//Clear inputs after submit
+	$("#InputEmail").val("");
+	$("#InputPassword").val("");
+	$("#InputName").val("");
+
+	//don't refresh page
+	return false;
+
+});
