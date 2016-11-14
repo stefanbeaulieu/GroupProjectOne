cf6642f696951aced04b1bc3a18d712054f44fc4
plelle <philip.lelle@gmail.com>
2016-11-14 11:21:42 -0500

    working js for product pull and showing

--- javascript/walmart.js
+++ javascript/walmart.js
@@ -38,33 +38,33 @@ function runQuery(queryURL){
 		console.log("-------------");
 
 		//Loop through and provide the correct amount of products
-		for (var i = 0; i < numResults.length; i++) {
+		for (var i = 0; i < wallData.items.length; i++) {
 
 			//increase productInfo by one each loop
 			productInfo++;
 
 			//Create HTML/Div well for product, price, description and price
-			var wellSection = ("<div>");
+			var wellSection = $('<div class="productWell">');
 			
-			wellSection.addClass('well');
-
 			//Display the title
-			wellSection.append('<p>' + wallData.response.items[i].name + "</p>");
+			var title = $('<p>' + wallData.items[i].name + '</p>');
 
 			//Display the product image
-			var image = $('<img>').attr("src", wallData.response.items[i].thumbnailImage);
-
-			//Append image
-			wellSection.append(image);
+			var image = $('<img>').attr('src', wallData.items[i].thumbnailImage);
 
 			//Display the price
-			wellSection.append($("<p>").html(wallData.response.items[i].salePrice));
+			var price = $('<p>' + wallData.items[i].salePrice + '</p>');
 
 			//Display the description
-			wellSection.append($("<p>").html(wallData.response.items[i].shortDescription));
+			var description = $('<p>' + wallData.items[i].longDescrption + '</p>');
 
 			//Append Div to home.html
-			('#prodSection').append(wellSection);
+			wellSection.append(title);
+			wellSection.append(image);
+			wellSection.append(price);
+			wellSection.append(description);
+
+			$('#prodSection').append(wellSection);
 
 
 			
@@ -87,13 +87,14 @@ function runQuery(queryURL){
 		var searchTerm = $('#addCategory').val().trim();
 		queryURL = "http://api.walmartlabs.com/v1/search?query=" + searchTerm + "&numItems=20&format=json&apiKey=wwrbpbwxmqp2me6d8hvubuhs&sort=price&order=asc";
 
-		$('#prodSection').append(searchTerm);
+		// $('#prodSection').append(wellSection);
 
 		//Set Min Price
 		var minPrice = $('#minPrice').val();
 
 		//Set Max Price
 		var maxPrice = $('#maxPrice').val();
+
 
 		//Pass info into parameters to run function
 		runQuery(queryURL);
